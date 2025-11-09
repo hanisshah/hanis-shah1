f1d700b5ec4f02d91fae941babda3b7ccc3fee4d
Hanis Ilya <hanisilya@gmail.com>
2025-11-09 17:56:30 +0800

    Change name and description in hero section
    Updated the introduction text and name in the hero section.

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,14 +60,13 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Hanis Ilya.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
+       Hanis is a Malaysia-based Data & Operations Analyst passionate about turning data into actionable insights. 
+       Experienced in Power BI, SQL, and Python, she enjoys visualizing complex data in simple, engaging ways.{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
         </a>
